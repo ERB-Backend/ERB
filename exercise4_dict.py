5d5f6fac378f90b60ab48799f590153296508af1
ERB-Backend <erbbackend@gmail.com>
2024-09-23 14:28:46 +0800

    rename files

---
+++ exercise4_dict.py
@@ -0,0 +1,29 @@
+# # Write a BMI calculator by user input
+# # BMI formula : Weight (kg) / Height(m)^2
+# #Hint:
+#
+# weight = float(input("Enter your weight"))
+# height = float(input("Enter your height in cm: "))
+# z = height **2
+# print("square of z is", z**2)
+
+# words = "Today is a beautiful sunny day! Today is also Sunday!"
+# word = words.split()
+# count = dict()
+#
+# for i in word:
+#     if i not in count:
+#         count[i]=1
+#     else:
+#         count[i]+=1
+# print(word)
+# print(count)
+
+test = {
+    "Car": 4,
+    "Bicycle": 2,
+    "Train": "many"
+        }
+
+for i in test:
+    print(i, "has", test[i], "wheels")
