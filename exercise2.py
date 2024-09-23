cc233cca01abbae958fc94a8f371d6bc4f1277c3
ERB-Backend <erbbackend@gmail.com>
2024-09-23 12:38:30 +0800

    create more exercises

--- exercise2.py
+++ exercise2.py
@@ -3,8 +3,20 @@ list1 = [5, "Peter", "Jenny", 3, 5, "Jenny"]
 # newList = set(list1)
 newList=[]
 
+# Method 1
 for i in list1:
     if i not in newList:
         newList.append(i)
 
 print(newList)
+
+# Method 2
+for i in list1:
+    if i in newList:
+        pass
+    else:
+        newList.append(i)
+
+print(newList)
+
+
---
+++ exercise3.py
@@ -0,0 +1,25 @@
+# n = 10
+# while True:
+#     print (n, end=' ')
+#     n = n-1
+#     if n == 0:
+#         break
+# print("Done")
+
+# myInput = 1
+#
+# while myInput!=0:
+#     myInput = int(input("Input an integer\n"))
+#     print("Your number times 10 is", myInput*10)
+#     break
+#
+
+# x =1
+# while x<11:
+#     print(x, end =" ")
+#     x+=1
+
+x =11
+while x>0:
+    print(x *"*")
+    x-=1
